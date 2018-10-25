04f6b0ef9e3fa80a5477a501fcd16218c60456ca
IFOV37 <kos37ter96@hotmail.com>
2018-10-24 23:40:24 -0400

    setup vars in ct1

--- projects/kosterkl/dominion/cardtest1.c
+++ projects/kosterkl/dominion/cardtest1.c
@@ -16,9 +16,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/************************************************************
+					SMITHY
+
+Expected behavior: Draw (+) 3 cards from the user's deck
+
+Input:
+	int currentPlayer,
+	int handPos,
+	struct gameState *state
+
+Output:
+	int (return 0)
+************************************************************/
+
+#define TESTCARD "Smity"
+
 
 int main(int argc, char* argv[])
 {
+	// setup our variables/things necessary to start and play the game/card
+    struct gameState state;
+    int k[10] = {adventurer, council_room, cutpurse, embargo, gardens, minion, mine, village, smithy, tribute};
+    int numPlayers = 2;
+    int handPosition = 0;
+    int seed = 1000;
+    int choice1, choice2, choice3, bonus = 0;		// no choices or bonus for smithy card
+
+	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
+	fflush(stdout);
+
+
+
+
+
 
 	return 0;
 }
