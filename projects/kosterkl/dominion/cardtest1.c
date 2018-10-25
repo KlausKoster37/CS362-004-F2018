aa085eda73a2e82c1d59cc2fdd33a729de83b259
IFOV37 <kos37ter96@hotmail.com>
2018-10-24 22:26:56 -0400

    added card test 1 through 4 initial files. all need work

---
+++ projects/kosterkl/dominion/cardtest1.c
@@ -0,0 +1,25 @@
+/*******************************************************************
+* Assignment 3: Unit Testing
+* File: cardtest1.c
+* Card in Test: smithy
+*
+* Author: Klaus Koster (kosterkl)
+* Due Date: 10/28/2018
+* CS 362, Fall 2018
+*******************************************************************/
+
+// include files
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "interface.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
+
---
+++ projects/kosterkl/dominion/cardtest3.c
@@ -0,0 +1,25 @@
+/*******************************************************************
+* Assignment 3: Unit Testing
+* File: cardtest3.c
+* Card in Test: council room
+*
+* Author: Klaus Koster (kosterkl)
+* Due Date: 10/28/2018
+* CS 362, Fall 2018
+*******************************************************************/
+
+// include files
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "interface.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
+
